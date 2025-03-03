857a4b80d000af375556088e3346492771f46be3
DazzlerSoftware001 <info@dazzlersoftware.com>
2025-03-03 16:42:49 +0530

    Update recruiter.php

--- routes/recruiter.php
+++ routes/recruiter.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\Recruiter\DashboardController;
 
 Route::prefix('Recruiter')->group(function () {
     Route::get('/Dashboard', [DashboardController::class, 'dashboard'])->name('Recruiter.dashboard');
-    Route::get('/jobpost', [DashboardController::class, 'jobpost'])->name('Recruiter.jobpost');
 });
