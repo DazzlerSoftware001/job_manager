2564bca9c413117b980d9b5383db9ff24202dcbc
DazzlerSoftware001 <info@dazzlersoftware.com>
2025-03-05 12:31:24 +0530

    f
    ff

--- app/Http/Controllers/Recruiter/JobController.php
+++ app/Http/Controllers/Recruiter/JobController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Recruiter;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\JobTypes;
+
 
 class JobController extends Controller
 {
     public function jobpost() {
-        return view('recruiter.Jobpost');
+        $data['jobTypes'] = JobTypes::where('status', 1)->select('type','status')->get(); 
+        // dd($data);
+        return view('recruiter.Jobpost',$data);
     }
 }
