33abcc49236861ca7e4826c84cd89f138f6f6972
DazzlerSoftware001 <info@dazzlersoftware.com>
2025-05-19 16:01:40 +0530

    f

--- app/Exports/TableExport.php
+++ app/Exports/TableExport.php
@@ -2,15 +2,39 @@
 
 namespace App\Exports;
 
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TableExport implements FromCollection
+class TableExport implements FromCollection, WithHeadings
 {
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+    }
+
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * Fetch data from the specified table
+     *
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        //
+        return DB::table($this->table)->get();
+    }
+
+    /**
+     * Return column headings
+     *
+     * @return array
+     */
+    public function headings(): array
+    {
+        // Get the first row to extract column names
+        $firstRow = (array) DB::table($this->table)->first();
+
+        return array_keys($firstRow);
     }
 }
