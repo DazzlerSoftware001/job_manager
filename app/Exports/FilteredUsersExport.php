555716977a680a4ad920e9e315c8beb2cca09b0e
DazzlerSoftware001 <info@dazzlersoftware.com>
2025-06-02 12:05:35 +0530

    Update FilteredUsersExport.php

--- app/Exports/FilteredUsersExport.php
+++ app/Exports/FilteredUsersExport.php
@@ -56,8 +56,8 @@ class FilteredUsersExport implements FromCollection, WithHeadings
         //     $query->whereJsonContains('skills', $this->filters['skills']);
         // }
 
-        if (! empty($skills) && is_array($skills)) {
-            $query->whereHas('user.candidateProfile', function ($q) use ($skills) {
+        if (!empty($skills) && is_array($skills)) {
+            $query->whereHas('candidateProfile', function ($q) use ($skills) {
                 $q->where(function ($q2) use ($skills) {
                     foreach ($skills as $skill) {
                         $q2->orWhere('skill', 'like', '%' . $skill . '%');
